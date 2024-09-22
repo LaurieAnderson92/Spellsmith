98fbe6d763d59e2f97b14aac92ebcd5880e1cac8
Laurie Anderson <laurieanderson92@hotmail.co.uk>
2024-09-22 21:16:19 +0000

    build: created detailed view

--- spellbook/urls.py
+++ spellbook/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.SpellList.as_view(), name='home'),
+    path('<int:id>/', views.spell_detail, name='spell_detail'),
 ]
--- spellbook/views.py
+++ spellbook/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Spell
 
@@ -7,3 +7,26 @@ from .models import Spell
 class SpellList(generic.ListView):
     model = Spell
     template_name = "spellbook/spell_list.html"
+
+def spell_detail(request, id):
+    """
+    Display an individual spell.
+
+    **Context**
+
+    ``spell``
+        An instance of a spell model.
+
+    **Template:**
+
+    :template:`spellbook/spell_detail.html`
+    """
+
+    queryset = Spell.objects.all()
+    spell = get_object_or_404(queryset, id=id)
+
+    return render(
+        request,
+        "spellbook/spell_detail.html",
+        {"spell": spell},
+    )
