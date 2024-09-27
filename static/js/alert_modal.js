810597ce418438d226ce82a27752aa27fe0d677a
Laurie Anderson <laurieanderson92@hotmail.co.uk>
2024-09-27 23:52:05 +0000

    fix: made navbar responsive #33

--- static/js/alert_modal.js
+++
@@ -1,3 +0,0 @@
-const deleteModal = document.getElementById("deleteConfirmationModal");
-const deleteButton = document.getElementsByClassName('.btn-delete');
-const deleteConfirm = document.getElementById("confirmDeleteBtn");
