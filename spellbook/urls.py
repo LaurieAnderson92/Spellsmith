fd90b3e082b8229152735f387311feea4e8ed6fd
Laurie Anderson <laurieanderson92@hotmail.co.uk>
2024-09-27 23:14:55 +0000

    fix: cleaned the spell detail URL

--- spellbook/urls.py
+++ spellbook/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.SpellList.as_view(), name='home'),
-    path('<int:id>/', views.spell_detail, name='spell_detail'),
+    path('spell/<int:id>/', views.spell_detail, name='spell_detail'),
     path('spell/create', views.spell_create, name='spell_create'),
     path('spell/edit/<int:id>/', views.spell_edit, name='spell_edit'),
     path('spell/delete/<int:id>/', views.spell_delete, name='spell_delete')
