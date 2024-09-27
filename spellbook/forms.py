35d578c9f5f2e4144d307f986d879e75b4a6de80
Laurie Anderson <laurieanderson92@hotmail.co.uk>
2024-09-27 23:14:10 +0000

    build: added feedback for form validation

--- spellbook/forms.py
+++ spellbook/forms.py
@@ -7,3 +7,9 @@ class SpellForm(forms.ModelForm):
         model = Spell
         fields = ['name','excerpt','school','ap_cost','mp_cost','range','duration',
                     'concentration','description','description','ap_enhancements','mp_enhancements']
+        
+        # This adds form validation feedback on submit
+        widgets = {
+            'ap_cost': forms.NumberInput(attrs={'min':1, 'max':4}),
+            'mp_cost': forms.NumberInput(attrs={'min':0, 'max':10})
+        }
