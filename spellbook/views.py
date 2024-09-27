713f8cd59f2a8ec1b1315a45b6f605c17c4302ae
Laurie Anderson <laurieanderson92@hotmail.co.uk>
2024-09-27 19:08:37 +0000

    build: added message alert system #28

--- spellbook/views.py
+++ spellbook/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render, get_object_or_404, HttpResponseRedirect, redirect
 from django.views import generic
+from django.contrib import messages
 from django.urls import reverse
 from .models import Spell
 from .forms import SpellForm
 
-# Create your views here.
 
+# Homepage 
+# Uses Django Generic list view
 class SpellList(generic.ListView):
     model = Spell
     template_name = "spellbook/spell_list.html"
 
+
+# Detail Page, 
+# It is passed the detail of a spell a displays it in a format that matches a DC20 spell template
 def spell_detail(request, id):
     """
     Display an individual spell.
@@ -33,6 +38,9 @@ def spell_detail(request, id):
         {"spell": spell},
     )
 
+
+# Create Page 
+# A page that uses a Crispy Form to take a new spell and POST it to the database
 def spell_create(request):
     """
     A function that displays the spell_create
@@ -45,9 +53,18 @@ def spell_create(request):
             spell = spell_form.save(commit=False)
             spell.creator = request.user
             spell.save()
+
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Your spell has been scribed into the communual spellbook!'
+            )
             id = spell.id
             return HttpResponseRedirect(reverse('spell_detail', kwargs={'id': id}))
-
+        else:
+            messages.add_message(
+                request, messages.Error,
+                'Error creating a spell, Please create a bug report <a href="https://github.com/LaurieAnderson92/Spellsmith/issues/new?assignees=&labels=&projects=&template=bug_report.md&title="> here </a>'           
+            )
     return render(
         request,
         "spellbook/spell_create.html",
@@ -59,18 +76,24 @@ def spell_edit(request, id):
     View to edit spells
     """
     spell = get_object_or_404(Spell, pk=id)
-    if request.method == "POST":
-        spell_form = SpellForm(data=request.POST, instance=spell)
-        if spell_form.is_valid():
-            spell.save()
-            return HttpResponseRedirect(reverse('spell_detail', kwargs={'id': id}))
-    spell_form = SpellForm(instance=spell)
-    
-    return render(
-        request,
-        "spellbook/spell_create.html",
-        {"spell_form": spell_form}
-    )
+    if request.user == spell.creator:
+        if request.method == "POST":
+            spell_form = SpellForm(data=request.POST, instance=spell)
+            if spell_form.is_valid():
+                spell.save()
+                return HttpResponseRedirect(reverse('spell_detail', kwargs={'id': id}))
+        spell_form = SpellForm(instance=spell)
+        
+        return render(
+            request,
+            "spellbook/spell_create.html",
+            {"spell_form": spell_form}
+        )
+    messages.add_message(
+                request, messages.WARNING,
+                'You do not have permission to edit this spell'           
+            )
+    return HttpResponseRedirect(reverse('home'))
 
 def spell_delete(request, id):
     """
@@ -80,5 +103,15 @@ def spell_delete(request, id):
 
     if spell.creator == request.user:
         spell.delete()
+        messages.add_message(
+                request, messages.SUCCESS,
+                'The spell has been successfully removed'           
+            )
+        return redirect('home')
 
-    return redirect('home')
+    else:
+        messages.add_message(
+            request, messages.WARNING,
+            'You do not have permission to delete this spell'            
+            )
+        return redirect('home')
