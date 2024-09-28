88221b1f617e949b4d142d65bc053c70b82b9fa9
Laurie Anderson <laurieanderson92@hotmail.co.uk>
2024-09-28 18:46:26 +0000

    build: completed footer

--- spellsmith/settings.py
+++ spellsmith/settings.py
@@ -27,7 +27,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = (os.environ.get("SECRET_KEY"))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-laurieanders-spellsmith-5o4sb6knrir.ws.codeinstitute-ide.net','.herokuapp.com']
 
